4d9182cf7e25cb465d412f4c327e2b88b70bc831
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2022-03-27 11:42:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography
+} from '@mui/material';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState([]);
+
+  const fetchProducts = async (query) => {
+    setSearchQuery(query);
+    if (query != undefined && query.length >= 3) {
+      const { data } = await Axios.get(
+        "http://localhost:8080/api/v1/productservice/searchProductDetails?query=" + query
+      );
+      const products = data;
+      setProducts(products);
+    } else {
+      setProducts([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      background: "#ffffff",
+      paddingTop: 128,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: 'column',
+    }}>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Search Products..."
+          variant="outlined"
+          onChange={(e) => fetchProducts(e.target.value)}
+          style={{ width: 600 }}
+        />
+      </div>
+      {
+        products.length > 0 ?
+          <div>
+            <Card style={{
+              width: 598,
+              border: "1px solid #BEBEBE",
+              margin: "12px",
+              boxShadow: "none"
+            }}>
+              <List>
+                {
+                  products.map((product, index) => {
+                    return (
+                      <ListItem disablePadding>
+                        <ListItemButton>
+                          <ListItemText
+                            primary={product.productName}
+                            secondary={
+                              <React.Fragment>
+                                <Typography
+                                  sx={{ display: 'inline' }}
+                                  component="span"
+                                  variant="body2"
+                                  color="text.primary"
+                                  style={{ marginRight: 12 }}>
+                                  INR {product.productPrice}
+                                </Typography>
+                                {product.productDescription}
+                              </React.Fragment>
+                            }
+                          />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })
+                }
+              </List>
+            </Card>
+          </div>
+          :
+          searchQuery.length > 0
+            ?
+            <p>Product Not Found</p>
+            :
+            null
+      }
     </div>
   );
 }
