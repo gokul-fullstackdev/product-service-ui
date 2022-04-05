ad4a198a8fe0d7b012b4f2fed4548b93be3be298
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2022-04-05 10:35:44 +0530

    Add new products from UI

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import './App.css';
 import React, { useState } from "react";
 import Axios from "axios";
 import {
+  Button,
   Card,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  InputAdornment,
+  Slide,
   TextField
 } from '@mui/material';
 
 import ProductSearchList from './components/ProductSearchList.js';
 import ProductNotFound from './components/ProductNotFound';
 import { SearchQuery } from './constant/ApplicationConstants';
+import { AddShoppingCartOutlined } from '@mui/icons-material';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 
 function App() {
-
   const [products, setProducts] = useState([]);
   const [searchQuery, setSearchQuery] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [productId, setProductId] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productPrice, setProductPrice] = useState(0);
 
   const fetchProducts = async (query) => {
     setSearchQuery(query);
@@ -28,6 +45,36 @@ function App() {
     }
   };
 
+  const saveProduct = async () => {
+    if (productName != undefined && productName.length >= 0
+      && productDescription != undefined && productDescription.length >= 0
+      && productPrice != undefined && productPrice >= 0) {
+      await Axios.post(
+        "http://localhost:8080/api/v1/productservice/saveProductDetails",
+        {
+          productId: productId,
+          productName: productName,
+          productDescription: productDescription,
+          productPrice: productPrice
+        }
+      );
+
+      setProductId("");
+      setProductName("");
+      setProductDescription("");
+      setProductPrice();
+    }
+    setOpen(false);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div style={{
       background: "#ffffff",
@@ -37,6 +84,87 @@ function App() {
       alignItems: "center",
       flexDirection: 'column',
     }}>
+      <div style={{
+        paddingBottom: 16,
+        width: '598px',
+        display: "flex",
+        flexDirection: 'column',
+      }}>
+        <Button
+          onClick={handleClickOpen}
+          variant="contained"
+          disableElevation
+          color='grey'
+          startIcon={<AddShoppingCartOutlined />}
+          style={{
+            alignItems: "flex-end",
+            alignContent: "flex-end",
+            justifyContent: "flex-end",
+            alignSelf: "flex-end",
+          }}>
+          Add New Product
+        </Button>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>{"Add New Product"}</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="id"
+              label="Product ID"
+              type="text"
+              fullWidth
+              variant="outlined"
+              value={productId}
+              onChange={(e) => setProductId(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              id="name"
+              label="Product Name"
+              type="text"
+              fullWidth
+              variant="outlined"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              id="description"
+              label="Product Description"
+              type="text"
+              fullWidth
+              variant="outlined"
+              multiline
+              rows={4}
+              value={productDescription}
+              onChange={(e) => setProductDescription(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              id="price"
+              label="Product Price"
+              type="number"
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: <InputAdornment position="start">INR</InputAdornment>
+              }}
+              value={productPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={saveProduct}>Save</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
       <div>
         <TextField
           id="outlined-basic"
